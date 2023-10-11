2a2a049a9b3532c6160f205c0cf60a687c18905b
yoshida <rsna6ce@aol.com>
2023-10-12 00:35:47 +0900

    updated functions.

--- laptime_gate.ino
+++ laptime_gate.ino
@@ -13,8 +13,8 @@
 #define PIN_UP   23
 #define PIN_IR   25
 
-#define BUTTON_DOWN HIGH
-#define BUTTON_UP   LOW
+#define BUTTON_DOWN LOW
+#define BUTTON_UP   HIGH
 #define IR_DETECTED HIGH
 #define IR_NODETECT LOW
 
@@ -25,6 +25,30 @@ TM1637Display display7seg(CLK, DIO);
 uint8_t data7seg[] = { 0xff, 0xff, 0xff, 0xff }; // all '1'
 
 LCD_I2C lcd(0x27, 20, 4);
+
+enum state {
+    state_stop,
+    state_ready,
+    state_run,
+    stete_finish};
+enum state state_mode = state_stop;
+
+typedef struct laptime_ {
+  uint32_t count;
+  uint32_t time_ms;
+  uint32_t lap_ms;
+} laptime;
+
+#define LAPTIME_COUNT_MAX 100
+laptime laptime_list[LAPTIME_COUNT_MAX] = {0};
+uint32_t laptime_count = 0;
+
+const uint32_t button_count_detect = 5;
+uint32_t button_count_run = 0;
+uint32_t button_count_stop = 0;
+uint32_t button_count_up = 0;
+uint32_t button_count_down = 0;
+uint32_t cursor_lap_index = 0;
 
 // ConvStr function from(https://synapse.kyoto/)
 String ConvStr(String str)
@@ -101,48 +125,227 @@ void setup() {
     lcd.clear();
     lcd.backlight();
     lcd.setCursor(0, 0);
-    lcd.print(ConvStr("ｼﾞｭﾝﾋﾞﾁｭｳ...  "));
+    lcd.print(ConvStr("LAPTIME GATE Ver0.1"));
     lcd.setCursor(0, 1);
-    lcd.print(ConvStr("ｼﾊﾞﾗｸｵﾏﾁｸﾀﾞｻｲ"));
+    lcd.print(ConvStr("　"));
     lcd.setCursor(0, 2);
-    lcd.print(ConvStr("Hello"));
+    lcd.print(ConvStr("ｽﾀｰﾄﾎﾞﾀﾝ ｦ ｵｼﾃｸﾀﾞｻｲ"));
     lcd.setCursor(0, 3);
-    lcd.print(ConvStr("world"));
+    lcd.print(ConvStr("　"));
 
-    xTaskCreatePinnedToCore(loop2, "loop2", 4096, NULL, 1, NULL, 0);
+    state_mode = state_stop;
+    display7seg.showNumberDecEx(0, 0x40, true);
+
+    xTaskCreatePinnedToCore(loop2, "loop2", 4096, NULL, 2, NULL, 0);
+    xTaskCreatePinnedToCore(loop3, "loop3", 4096, NULL, 1, NULL, 0);
     Serial.println("setup finished.");
 }
 
+// 7segment manuary
+//     a0
+//  f5    b1
+//     g6
+//  e4    c2
+//     d3
+void display_7seg_stop(){
+    data7seg[0] = (1<<0) | (0<<1) | (1<<2) | (1<<3) | (0<<4) | (1<<5) | (1<<6); // S
+    data7seg[1] = (0<<0) | (0<<1) | (0<<2) | (1<<3) | (1<<4) | (1<<5) | (1<<6); // t
+    data7seg[2] = (0<<0) | (0<<1) | (1<<2) | (1<<3) | (1<<4) | (0<<5) | (1<<6); // o
+    data7seg[3] = (1<<0) | (1<<1) | (0<<2) | (0<<3) | (1<<4) | (1<<5) | (1<<6); // p
+    display7seg.setSegments(data7seg);
+}
+
+static uint32_t count_for_display = 0;
+static uint32_t laptime_count_latest = 0;
+
+static const uint32_t gap_ignore_time = 300;
+static const uint32_t laptime_hold_time = 700;
+
+// loop for laptime
 void loop2(void * params) {
+    uint8_t ir_prev = digitalRead(PIN_IR);
+    uint8_t ir_curr = ir_prev;
+    uint32_t millis_curr = 0;
+    uint32_t millis_run_started = 0;
+    uint32_t millis_lap_started = 0;
+    uint32_t millis_tail_started = 0;
+    uint32_t millis_laptime_hold = 0;
     while (true) {
-        delay(1000);
-        Serial.print("IR ");
-        Serial.println(digitalRead(PIN_IR));
-        Serial.print("START ");
-        Serial.println(digitalRead(PIN_START));
-        Serial.print("STOP ");
-        Serial.println(digitalRead(PIN_STOP));
-        Serial.print("DOWN ");
-        Serial.println(digitalRead(PIN_DOWN));
-        Serial.print("UP ");
-        Serial.println(digitalRead(PIN_UP));
+        ir_prev = ir_curr;
+        ir_curr = digitalRead(PIN_IR);
+        millis_curr = (uint32_t)millis();
+        if (state_mode == state_run) {
+            if (ir_prev==IR_NODETECT && ir_curr==IR_DETECTED) {
+                uint32_t elapsed_from_tail = millis_curr - millis_tail_started;
+                if (gap_ignore_time < elapsed_from_tail) {
+                    // append recorde
+                    uint32_t laptime_index = laptime_count;
+                    laptime_list[laptime_index].count = laptime_index + 1;
+                    laptime_list[laptime_index].time_ms = (uint32_t)(millis_curr-millis_run_started);
+                    laptime_list[laptime_index].lap_ms = (uint32_t)(millis_curr-millis_lap_started);
+                    laptime_count++;
+                    if (LAPTIME_COUNT_MAX <= laptime_count) {
+                        // overflow finish
+                        state_mode = state_stop;
+                        cursor_lap_index = laptime_count;
+                    }
+                    millis_lap_started = millis_curr;
+                    millis_laptime_hold = millis_curr + laptime_hold_time;
+                }
+            } else if (ir_prev==IR_DETECTED && ir_curr==IR_NODETECT) {
+                millis_tail_started = millis_curr;
+            }
+            if (millis_laptime_hold < millis_curr) {
+                count_for_display = (uint32_t)(millis_curr-millis_lap_started)/10;
+            }
+        } else if ( state_mode == state_ready) {
+            if (ir_prev==IR_NODETECT && ir_curr == IR_DETECTED) {
+                millis_run_started = millis_curr;
+                millis_lap_started = millis_curr;
+                millis_tail_started = millis_curr;
+                laptime_count = 0;
+                laptime_count_latest = 0;
+                state_mode = state_run;
+            }
+        }
+        delay(2);
     }
 }
 
-static uint32_t count = 0;
-void loop() {
-    count++;
-    
-    if(count&1) {
-        data7seg[0] = count >= 1000 ? display7seg.encodeDigit((count / 1000)) : 0;
-        data7seg[1] = display7seg.encodeDigit((count % 1000) / 100);
-        data7seg[2] = display7seg.encodeDigit((count %  100) /  10);
-        data7seg[3] = display7seg.encodeDigit((count %   10));
-        display7seg.setSegments(data7seg);
-    } else {
-        display7seg.showNumberDecEx(count, 0x40, true);
+// loop for display7seg
+void loop3(void * params) {
+    uint32_t count_latest = 0;
+    enum state state_mode_prev = state_stop;
+    while (true) {
+        if (state_mode == state_run) {
+            display7seg.showNumberDecEx(count_for_display, 0x40, true);
+        } else if (state_mode_prev != state_mode) {
+            state_mode_prev = state_mode;
+            if (state_mode == state_stop) {
+                display_7seg_stop();
+            } else if (state_mode == state_ready) {
+                display7seg.showNumberDecEx(0, 0x40, true);
+            }
+        }
+        delay(5);
     }
-    
-    digitalWrite(INNER_LED, count&1);
+}
+
+void display_lcd_lap_latest(int count) {
+    lcd.clear();
+    for (int i=0; i<4; i++) {
+        // format '99 T:000.00 L:000.00'
+        int index = count-1-i;
+        if (index < 0) {
+            //skip
+        } else {
+            char buf_all[24];
+            char buf_time[8];
+            char buf_laps[8];
+            double time_10ms = (double)(laptime_list[index].time_ms/10) * 0.01;
+            double lap_10ms = (double)(laptime_list[index].lap_ms/10) * 0.01;
+            dtostrf(time_10ms, 6, 2, buf_time);
+            dtostrf(lap_10ms, 6, 2, buf_laps);
+            sprintf(buf_all,"%02d T:%s L:%s",(int)laptime_list[index].count, buf_time, buf_laps);
+            lcd.setCursor(0, i);
+            lcd.print(String(buf_all));
+        }
+    }
+}
+
+void display_lcd_lap_record(int cursor) {
+    lcd.clear();
+    for (int i=0; i<4; i++) {
+        // format '99 T:000.00 L:000.00'
+        int index = cursor-i;
+        if (index < 0) {
+            // skip
+        } else {
+            char buf_all[24];
+            char buf_time[8];
+            char buf_laps[8];
+            double time_10ms = (double)(laptime_list[index].time_ms/10) * 0.01;
+            double lap_10ms = (double)(laptime_list[index].lap_ms/10) * 0.01;
+            dtostrf(time_10ms, 6, 2, buf_time);
+            dtostrf(lap_10ms, 6, 2, buf_laps);
+            sprintf(buf_all,"%02d T:%s L:%s",(int)laptime_list[index].count, buf_time, buf_laps);
+            lcd.setCursor(0, i);
+            lcd.print(String(buf_all));
+        }
+    }
+}
+
+//loop main
+void loop() {
+    // button events with chattering workaround
+    // button run
+    if (digitalRead(PIN_START)==BUTTON_DOWN) {
+        if (++button_count_run == button_count_detect) {
+            // stop -> start
+            if (state_mode == state_stop) {
+                state_mode = state_ready;
+                lcd.clear();
+                lcd.setCursor(0, 0);
+                lcd.print(ConvStr("ｼﾞｭﾝﾋﾞOK!!"));
+                lcd.setCursor(0, 1);
+                lcd.print(ConvStr(" "));
+                lcd.setCursor(0, 2);
+                lcd.print(ConvStr("ｹﾞｰﾄﾂｳｶﾃﾞ"));
+                lcd.setCursor(0, 3);
+                lcd.print(ConvStr("ｼﾞﾄﾞｳｹｲｿｸｶｲｼｼﾏｽ"));
+            }
+        }
+    } else {
+        button_count_run = 0;
+    }
+    // button stop
+    if (digitalRead(PIN_STOP)==BUTTON_DOWN) {
+        if (++button_count_stop == button_count_detect) {
+            if (state_mode == state_run || state_mode == state_ready) {
+                state_mode = state_stop;
+                cursor_lap_index = laptime_count-1;
+            }
+        }
+    } else {
+        button_count_stop = 0;
+    }
+    // buton up
+    if (digitalRead(PIN_UP)==BUTTON_DOWN) {
+        if (++button_count_up == button_count_detect) {
+            if (state_mode == state_stop) {
+                Serial.print("cursor_lap_index"); Serial.println(cursor_lap_index);
+                Serial.print("laptime_count"); Serial.println(laptime_count);
+                if (cursor_lap_index+4 < laptime_count) {
+                    cursor_lap_index += 4;
+                }
+                display_lcd_lap_record(cursor_lap_index);
+            }
+        }
+    } else {
+        button_count_up = 0;
+    }
+    // button down
+    if (digitalRead(PIN_DOWN)==BUTTON_DOWN) {
+        if (++button_count_down == button_count_detect) {
+            if (state_mode == state_stop) {
+                if (4 <= cursor_lap_index) {
+                    cursor_lap_index -= 4;
+                }
+                display_lcd_lap_record(cursor_lap_index);
+            }
+        }
+    } else {
+        button_count_down = 0;
+    }
+
+    //display lcd
+    if (state_mode == state_run) {
+        if (laptime_count_latest < laptime_count) {
+            laptime_count_latest = laptime_count;
+            display_lcd_lap_latest(laptime_count_latest);
+        } else if (laptime_count == 0) {
+            lcd.clear();
+        }
+    }
     delay(10);
 }
